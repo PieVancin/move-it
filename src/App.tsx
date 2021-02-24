5a8df55937d414f9df8a0ea91d4f127753b84e66
Pierre <pie.vancin@gmail.com>
2021-02-24 19:38:15 -0300

    gitignore atualizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,8 @@
-import { Button } from'./components/Button' //Importa o button dentro do componente App
-
 import './styles/global.css';
 
 function App() {
   return (
-    <div>Hello world
-    <p><Button color="blue">
-      Botão 1
-    </Button>
-    </p>
-    <Button color="green">
-      Botão 2
-    </Button>
-    </div>
+    <div>Hello world</div>
   );
 }
 
